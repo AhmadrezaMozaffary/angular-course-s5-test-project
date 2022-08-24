3ddb7abc148dcc334413dd6989c77c95da812885
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-24 16:19:41 +0430

    Reference to a DOM element deirectly from TS code using @ViewChild decoraor and Local reference

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
   selector: "app-cockpit",
@@ -10,30 +17,32 @@ export class CockpitComponent implements OnInit {
     serverName: string;
     serverContent: string;
   }>();
-  
+
   @Output() blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
 
-  newServerName = "";
-  newServerContent = "";
+  // newServerName = "";
+  // newServerContent = "";
+
+  @ViewChild("serverContentInput", { static: true })
+  serverContentInput: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onAddServer(serverNameInput: HTMLInputElement){
+  onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName:serverNameInput.value,
-      serverContent: this.newServerContent
-    })
+      serverName: serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
   }
-  onAddBlueprint(serverNameInput: HTMLInputElement){
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent
-    })
+      serverContent:  this.serverContentInput.nativeElement.value,
+    });
   }
-
 }
